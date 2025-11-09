04360590e279edfa10937f9cf81e0a2272ade5d8
MichaelHo <michelo851a1203@gmail.com>
2025-11-09 08:21:14 +0800

    feat(): try app runner # 2025-11-09-002
    	modified:   apprunner.yaml
    	modified:   main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import uvicorn
+import os
 
 app = FastAPI(title="demo app", description="demo app sample", version="1.0.0")
 
@@ -8,7 +9,13 @@ def root():
     return { "status": "OK" }
 
 def main():
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(
+            app, 
+            host="0.0.0.0", 
+            port=8080, 
+            log_level=os.getenv("Log_LEVEL", "info"),
+            proxy_headers=True, 
+        )
 
 if __name__ == "__main__":
     main()
